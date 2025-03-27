7d75a550a4c4156c8ad25885c89978138e56f48b
Osirimu <amodederinsola@gmail.com>
2025-03-27 14:30:02 +0000

    first commit

---
+++ demo.py
@@ -0,0 +1,27 @@
+# total_serves = 5
+# active_servers = 3
+# offline_servers = total_serves - active_servers
+
+# print(f"Total servers in cluster: {total_serves}")
+# print(f"Active servers: {active_servers}")
+# print(f"Offline servers: {offline_servers}")
+
+
+# item_price = 29.99
+# quantity = 3
+# total_cost = item_price * quantity
+# print(f"Total cost: ${total_cost}")
+
+
+addition = 2 + 3
+subtraction = 8 - 2
+division = 6 / 2
+multiplication = 2 * 4
+
+exponent = 2 ** 3
+floor_division =  17 // 5
+modulo = 17 % 5
+
+combined = (4 + 3) * 2
+
+print(f"total: {addition,subtraction,division,multiplication,exponent,floor_division,modulo,combined}")
