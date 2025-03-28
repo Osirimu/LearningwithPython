9269a5ef844618e5ba9087c63875159282805f47
Osirimu <amodederinsola@gmail.com>
2025-03-28 13:59:37 +0000

    updating

---
+++ Exercises/lesson_1_print_basic_strinfs.py
@@ -0,0 +1,3 @@
+#1. Print a short story using multiple line
+#TODO
+print("After series of tests conducted on the patient, our fears were confirmed!\nshe had Pneumonia and she was dying.\nwe have to gather 38 million for the treatment.")
---
+++ printing_with_python.py
@@ -0,0 +1,10 @@
+print("My first print statement")
+
+print("I am learning Python")
+print("This is so much fun!")
+print("I am loving this.")
+
+
+print("first line\nSecond line")
+print("This\tis\ttabbed")
+print("My name is", "Ade", "and I am an", "engineering student")
