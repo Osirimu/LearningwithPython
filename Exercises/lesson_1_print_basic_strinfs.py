f219f85a2455d371c2c9f1ef93c190ceb3e7ebcd
Osirimu <amodederinsola@gmail.com>
2025-03-28 14:18:00 +0000

    updated

--- Exercises/lesson_1_print_basic_strinfs.py
+++ Exercises/lesson_1_print_basic_strinfs.py
@@ -1,3 +1,17 @@
 #1. Print a short story using multiple line
 #TODO
 print("After series of tests conducted on the patient, our fears were confirmed!\nshe had Pneumonia and she was dying.\nwe have to gather 38 million for the treatment.")
+
+#2. Create a string that includes both single and double quotes.
+#TODO
+print("The lecturer asked, 'How are you guys doing?'")
+print('They replied, "Very well, thank you sir"')
+
+#3. Use escape characters to create a formatted list
+#TODO
+print("1.Item one\n -Subitem A\n -Subitem B\n")
+print("2.Item two\n -Subitem A\n -Subitem B\n.")
+
+#4. Combine diiferent types of data(strings and numbers) in one print statement
+#TODO
+print("There are 5 pieces of meat in the pot of stew. Take 1")
