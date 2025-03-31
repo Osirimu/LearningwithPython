235da7c905c93103167e793d15d3e79fc7d7b222
Osirimu <amodederinsola@gmail.com>
2025-03-31 21:12:57 +0100

    update

---
+++ python_comment_best_practice.py
@@ -0,0 +1,8 @@
+# This is a single-line comment
+x = 10  # Assigns the value of 10 to the variable x
+y = 20  # Assigns the value of 20 to the variable y
+
+# Calculate the sum of x + y
+result = x + y
+
+print(result)
