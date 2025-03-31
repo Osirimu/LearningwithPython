9b74c3e292eec31404cb546e713cf9d7c1bb681d
Osirimu <amodederinsola@gmail.com>
2025-03-31 21:00:16 +0100

    update

--- Exercises/lesson_1_multi_line_printing.py
+++ Exercises/lesson_1_multi_line_printing.py
@@ -1,23 +1,23 @@
-#Try printing a simple three-line text or poem using three simple seperate print() statements.
-#TODO
+# Try printing a simple three-line text or poem using three simple seperate print() statements.
+# TODO
 print("Twinkle, twinkle little star")
 print("How I wonder what you are")
 print("Up above the world so high...")
 print("\n")
-#Try printing a simple three-line text or poem using one print() statement with \n.
-#TODO
+# Try printing a simple three-line text or poem using one print() statement with \n.
+# TODO
 print("Twinkle, twinkle little star\nHow I wonder what you are\nUp above the world so high...")
 print("\n")
 
-#print it using triple quotes
-#TODO
+# print it using triple quotes
+# TODO
 print("""I see the moon
 the moon sees me
 God bless the moon and God bless me""")
 print("\n")
 
-#Add a blank line between each sting/verse of your text/poem using any method you prefer
-#TODO
+# Add a blank line between each sting/verse of your text/poem using any method you prefer
+# TODO
 print("""I see the moon\n
 the moon sees me\n
 God bless the moon and God bless me""")
