b8fbebfbd960e592acc8c359c3542d98183c0669
Osirimu <amodederinsola@gmail.com>
2025-04-05 17:41:56 +0100

    update

---
+++ Exercises/lesson2_data_type_identification.py
@@ -0,0 +1,18 @@
+# For each of the following values determine data types:
+# 42
+# 3.14
+# "Hello, World!"
+# True
+# -7
+# "42"
+# False
+
+
+#Solution
+print(type(42))
+print(type(3.14))
+print(type("Hello, World!"))
+print(type(True))
+print(type(-7))
+print(type("42"))
+print(type(False))
